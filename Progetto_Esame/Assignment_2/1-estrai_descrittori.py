c2ecaae201e73e38400ebcae0bbf45e71c063a1b
enricobarone761 <enribarone01+github@gmail.com>
2026-05-22 19:27:58 +0200

    feat: refactor descriptor extraction and KMeans training for improved clarity and flexibility

--- Progetto_Esame/Assignment_2/1-estrai_descrittori.py
+++ Progetto_Esame/Assignment_2/1-estrai_descrittori.py
@@ -10,34 +10,24 @@ def leggi_foto(cartella):
         if f.is_file():
             img = cv.imread(str(f), flags=cv.IMREAD_GRAYSCALE)
             if img is not None:# imread ritorna None se il file è corrotto
-                print(f"Image {f} read successfully.")
+                print(f"caricata immagine {f}")
                 yield img
 
 N_FEATURES = 300
 
 sift = cv.SIFT_create(nfeatures=N_FEATURES)
-sift_list = np.empty((N_FEATURES * 10_000, 128), dtype=np.float32)
+sift_list = []
 
-idx = 0
+for img in leggi_foto(PATH):
+    _, descrittori = sift.detectAndCompute(img, None)
+    if descrittori is not None: #and len(descriptors) > 0
+        cv.normalize(descrittori, descrittori, norm_type=cv.NORM_L2)
+        sift_list.extend(descrittori)
+        print(f"estratti {descrittori.shape[0]} descrittori dall'immagine")
 
-for file in leggi_foto(PATH):
-    keypoints, descriptors = sift.detectAndCompute(file, None)
-    if descriptors is not None: #and len(descriptors) > 0
-        cv.normalize(descriptors, descriptors, norm_type=cv.NORM_L2)
-        r,c = descriptors.shape
-        sift_list[idx:idx+r] = descriptors
-        idx += r
-        print(f"Descriptors shape: {r} x {c}, total so far: {idx}")
+print(f"Descrittori totali estratti: {len(sift_list)}")
 
-print(f"Total descriptors extracted: {len(sift_list)}")
-
-# Taglia solo la parte effettivamente usata
-sift_list = sift_list[:idx]
-
-#descriptors = np.vstack(sift_list)
-#cv.normalize(descriptors, descriptors, norm_type=cv.NORM_L2)
-
-with open(r'Progetto_Esame/Assignment_2/descrittori_300.pkl', 'wb') as f:
+with open(rf'Progetto_Esame/Assignment_2/descrittori_{N_FEATURES}.pkl', 'wb') as f:
     pickle.dump(sift_list, f)
 
-print("Descriptors saved to descrittori_300.pkl")
+print("Lista descrittori salvata")
--- Progetto_Esame/Assignment_2/2-clustering_vocabolario.py
+++ Progetto_Esame/Assignment_2/2-clustering_vocabolario.py
@@ -1,17 +1,21 @@
 import pickle
 from sklearn.cluster import KMeans
 
-with open(r'Progetto_Esame/Assignment_2/descrittori&vacabolario/descrittori_300.pkl', 'rb') as f:
-    descriptors = pickle.load(f)
+def train_kmeans_model(nfeatures):
+    with open(rf'Progetto_Esame/Assignment_2/descrittori&vacabolario/descrittori_{nfeatures}.pkl', 'rb') as f:
+        descriptors = pickle.load(f)
 
-K_VALUES = [50, 100, 500]
+    K_VALUES = [50, 100, 500]
 
-for K in K_VALUES:
-    print(f"Training KMeans con K={K}...")
-    kmeans = KMeans(n_clusters=K, random_state=42, verbose=1)
-    kmeans.fit(descriptors)
-    print(f"  {K}-means addestrato con inertia: {kmeans.inertia_:.2f}")
+    for K in K_VALUES:
+        print(f"Training KMeans con K={K}...")
+        kmeans = KMeans(n_clusters=K, random_state=42, verbose=1)
+        kmeans.fit(descriptors)
 
-    with open(rf'Progetto_Esame/Assignment_2/vocab_k{K}_300.pkl', 'wb') as f:
-        pickle.dump(kmeans, f)
-    print(f"  Vocabolario K={K} salvato in vocab_k{K}_300.pkl")
+        with open(rf'Progetto_Esame/Assignment_2/vocab_k{K}_{nfeatures}.pkl', 'wb') as f:
+            pickle.dump(kmeans, f)
+            print(f"Vocabolario K={K} salvato")
+
+
+if __name__ == "__main__":
+    train_kmeans_model(300)
